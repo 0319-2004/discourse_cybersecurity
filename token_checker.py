66d9cfcadfb8164287a0e90774bddd26f140ccc3
0319-2004 <liyins997@gmail.com>
2025-07-24 14:37:25 +0900

    Delete token_checker.py

--- token_checker.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def check_token(api_url, token):
-    headers = {"Authorization": f"Bearer {token}"}
-    response = requests.get(api_url, headers=headers)
-    print("Status Code:", response.status_code)
-    print("Response:", response.text)
